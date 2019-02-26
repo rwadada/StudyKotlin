436f0e1bd6a0aac46954b4842527247f7d7b041d
rwadada <wadada.knct@gmail.com>
2019-02-26 22:36:24 +0900

    2.1.4 より簡単な文字列のフォーマット：文字列テンプレート

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    println("Hello, world!")
-    println(max(1, 2))
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name")
 }
 
 fun max(a: Int, b: Int): Int = if (a > b) a else b
