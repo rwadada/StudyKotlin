30250f1b7d39f922c500dd531ba20c19537590f3
rwadada <wadada.knct@gmail.com>
2019-02-27 23:21:05 +0900

    2.3.7 ifやwhenの分岐先でのブロック

--- src/geometry/example/Main.kt
+++ src/geometry/example/Main.kt
@@ -28,6 +28,8 @@ fun main(args: Array<String>) {
 
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
 
+    println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
+
 }
 
 fun max(a: Int, b: Int): Int = if (a > b) a else b
@@ -80,6 +82,21 @@ fun eval(e: Expr): Int =
             is Num -> e.value
             is Sum -> {
                 eval(e.right) + eval(e.left)
+            }
+            else -> throw IllegalArgumentException("Unknown expression")
+        }
+
+fun evalWithLogging(e: Expr): Int =
+        when (e) {
+            is Num -> {
+                println("num: ${e.value}")
+                e.value
+            }
+            is Sum -> {
+                val left = evalWithLogging(e.left)
+                val right = evalWithLogging(e.right)
+                println("sum: $left + $right")
+                left + right
             }
             else -> throw IllegalArgumentException("Unknown expression")
         }
