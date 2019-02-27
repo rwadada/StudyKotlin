622e2fbf975b202e3b351ad663df51bf41a586f8
rwadada <wadada.knct@gmail.com>
2019-02-27 20:35:20 +0900

    2.3.2 whenを使ってenumクラスを処理する

--- src/geometry/example/Main.kt
+++ src/geometry/example/Main.kt
@@ -1,6 +1,7 @@
 package geometry.example
 
 import geometry.Color
+import geometry.Color.*
 import geometry.Person
 import geometry.shapes.Rectangle
 
@@ -14,7 +15,28 @@ fun main(args: Array<String>) {
     val rectangle = Rectangle(41, 43)
     println(rectangle.isSquare)
 
-    println(Color.BLUE.rgb())
+    println(BLUE.rgb())
+
+    println(getMnemonic(BLUE))
+    println(getWarms(ORANGE))
 }
 
 fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun getMnemonic(color: Color): String =
+        when (color) {
+            RED -> "Richard"
+            ORANGE -> "Of"
+            YELLOW -> "York"
+            GREEN -> "Gave"
+            BLUE -> "Battle"
+            INDIGO -> "In"
+            VIOLET -> "Vain"
+        }
+
+fun getWarms(color: Color): String =
+        when(color) {
+            RED, ORANGE, YELLOW -> "warm"
+            GREEN -> "neutral"
+            BLUE, INDIGO, VIOLET -> "cold"
+        }
