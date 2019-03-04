0ace2fc5d07014c434c955fb60b569105c3a4d05
rwadada <wadada.knct@gmail.com>
2019-03-04 23:48:11 +0900

    2.4.3 マップの繰り返し処理

--- src/geometry/example/Main.kt
+++ src/geometry/example/Main.kt
@@ -6,6 +6,7 @@ import geometry.Expr
 import geometry.Person
 import geometry.shapes.Rectangle
 import java.lang.Exception
+import java.util.*
 
 
 fun main(args: Array<String>) {
@@ -29,6 +30,17 @@ fun main(args: Array<String>) {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
 
     println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
+
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
 
 }
 
